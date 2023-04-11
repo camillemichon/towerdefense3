fd8a8a5a3cba250ec6c4908d741c95740fccd0d7
camille.michon <camille.michon@epita.fr>
2023-04-10 21:03:36 -0700

    part 3 done

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,43 +1,27 @@
 using UnityEngine;
-using System.Collections;
+using UnityEngine.AI;
 
 [RequireComponent(typeof(Enemy))]
 public class EnemyMovement : MonoBehaviour {
 
-	private Transform target;
-	private int waypointIndex = 0;
-
+	private Vector3 target;
 	private Enemy enemy;
+	private NavMeshAgent navMeshAgent;
 
 	private void Start()
 	{
 		enemy = GetComponent<Enemy>();
-		target = Waypoints.points[0];
+		navMeshAgent = GetComponent<NavMeshAgent>();
+		target = new Vector3(70, transform.position.y,-70);
+		navMeshAgent.destination = target;
 	}
 
 	private void Update()
 	{
-		Vector3 dir = target.position - transform.position;
-		transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
-
-		if (Vector3.Distance(transform.position, target.position) <= 0.4f)
-		{
-			GetNextWaypoint();
-		}
-
-		enemy.speed = enemy.startSpeed;
-	}
-
-	private void GetNextWaypoint()
-	{
-		if (waypointIndex >= Waypoints.points.Length - 1)
-		{
+		if (navMeshAgent.pathStatus == NavMeshPathStatus.PathComplete && navMeshAgent.remainingDistance == 0)
 			EndPath();
-			return;
-		}
-
-		waypointIndex++;
-		target = Waypoints.points[waypointIndex];
+		
+		enemy.speed = enemy.startSpeed;
 	}
 
 	private void EndPath()
